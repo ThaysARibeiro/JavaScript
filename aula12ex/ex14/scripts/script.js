1ff46d81ca456a2c7900008249768589cc45a060
Thays Ribeiro <thaysamandaribeiro@gmail.com>
2023-03-14 19:04:19 +0000

    Exercício 14

--- aula12ex/ex14/scripts/script.js
+++ aula12ex/ex14/scripts/script.js
@@ -6,11 +6,14 @@ function carregar() {
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12) {
-        img.src = 'foto-manha.png'
-    } else if (hora >= 12 && hora <18) {
-        img.src = 'foto-tarde.png'
+        img.src = 'imagens/foto-manha.png'
+        document.body.style.backgroundColor = '#fffeaf'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/foto-tarde.png'
+        document.body.style.backgroundColor = '#fecc92'
     } else {
-        img.src = 'foto-noite.png'
+        img.src = 'imagens/foto-noite.png'
+        document.body.style.backgroundColor = '#c9876d'
     }
 }
     
