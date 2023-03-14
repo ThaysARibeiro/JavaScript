5b0d9df214fb8d26b800397bd5663eae596704b4
Thays Ribeiro <thaysamandaribeiro@gmail.com>
2023-03-14 18:49:01 +0000

    Organização Pastas

--- aula12ex/ex14/scripts/script.js
+++ aula12ex/ex14/scripts/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12) {
+        img.src = 'foto-manha.png'
+    } else if (hora >= 12 && hora <18) {
+        img.src = 'foto-tarde.png'
+    } else {
+        img.src = 'foto-noite.png'
+    }
+}
+    
+
+
